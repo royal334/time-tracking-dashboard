0b6d285f2b4519b22f3f47b75a69cc973c592470
Sam-Otuonye Royal <samotuonyeroyal@gmail.com>
2024-12-16 15:42:07 +0100

    done with the HTML and CSS

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+// fetch('/data.json').then(response => response.json()).then((json) => {console.log(json);
+//      console.log(json[0].timeframes.daily.current)
+// }).catch(error => console.error('Loading error'))
